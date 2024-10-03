43a7bdea05cad8a4633ad5db6ffb530c1406917d
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2024-10-03 16:26:41 -0300

    app de favoritos github - atualização

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,11 +1,8 @@
-
 import React, { useState, useCallback } from "react";
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
 import './styles.css';
-
 import api from '../../services/api';
-
 
 const Modal = ({ isOpen, onClose, message }) => {
   if (!isOpen) return null;
@@ -37,28 +34,25 @@ export default function Main() {
 
       setLoading(true);                // habilita o loading
       try {
+        
+        if (newRepo === '') {                                         // verificando se foi digitado algo
+          setErrorMessage('Você precisa indicar um repositório');     // modal
+          setIsModalOpen(true);    
 
-        // verificando se foi digitado algo
-        if (newRepo === '') {
-          setErrorMessage('Você precisa indicar um repositório');    // modal
-          setIsModalOpen(true);
-
-          return;
-          // throw new Error('Você precisa indicar um repositório');
+          throw new Error('Você precisa indicar um repositório');
         }
+       
+        const response = await api.get(`repos/${ newRepo }`)  // exemplo : https://api.github.com/repo/facebook/react
 
+        const hasRepo = repositorios.find(repo => repo.name === newRepo);
+        console.log(hasRepo);
 
-        // verificando se repo ja existe
-        const seExisteRepo = repositorios.find(repo => repo.name === newRepo);
-
-        if (seExisteRepo) {
+        if (hasRepo) {                                  // verificando se repo ja existe
           setErrorMessage('Repositorio Duplicado');
           setIsModalOpen(true);
-
-          throw new Error('Repositorio Duplicado');  // se tem o repositorio vou barrar
+          
+          throw new Error('Repositorio Duplicado');     // se tem o repositorio vou barrar
         }
-
-        const response = await api.get(`repos/${ newRepo }`)  // exemplo : https://api.github.com/repo/facebook/react
 
         const data = {                              // pega o q foi digitado   // desconstrução
           name: response.data.full_name,            // colocando dentro objeto para poder colocar mais depois
@@ -67,8 +61,8 @@ export default function Main() {
         setRepositorio([...repositorios, data]);    // pega tudo q tem e adiciona o data, novo cadastro
         setNewRepo('');                             // para limpar o input
 
-      } catch (err) {
-        console.log(err);
+      } catch (error) {
+        console.log(error);
 
       } finally {
         setLoading(false);                          // finaliza o loading
@@ -84,10 +78,11 @@ export default function Main() {
   }
 
   const handleDelete = useCallback((repo) => {
-    const encontrar = repositorios.filter(r => r.name !== repo);
-    setRepositorio(encontrar);    // find - retorna tudo menos o ue foi clicado para deletar
+    const encontrar = repositorios.filter(r => r.name !== repo); // filter - retorna tudo menos o ue foi clicado para deletar
+    setRepositorio(encontrar);    
 
-    alert(`Repositório ${ repo } Deletado!`)
+    setErrorMessage('Repositório Deletado!');
+    setIsModalOpen(true);
 
   }, [repositorios])
 
@@ -145,3 +140,146 @@ export default function Main() {
 
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useCallback, useEffect } from 'react';
+// import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
+// import { Container, Form, SubmitButton, List, DeleteButton } from './styles';
+
+// import api from '../../services/api';
+
+// export default function Main() {
+
+//   const [newRepo, setNewRepo] = useState('');
+//   const [repositorios, setRepositorios] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [alert, setAlert] = useState(null);
+
+//   // Buscar
+//   useEffect(() => {
+//     const repoStorage = localStorage.getItem('repos');
+
+//     if (repoStorage) {
+//       setRepositorios(JSON.parse(repoStorage));
+//     }
+
+//   }, []);
+
+
+//   // Salvar alterações
+//   useEffect(() => {
+//     localStorage.setItem('repos', JSON.stringify(repositorios));
+//   }, [repositorios]);
+
+//   const handleSubmit = useCallback((e) => {
+//     e.preventDefault();
+
+//     async function submit() {
+//       setLoading(true);
+//       setAlert(null);
+//       try {
+
+//         if (newRepo === '') {
+//           throw new Error('Você precisa indicar um repositorio!');
+//         }
+
+//         const response = await api.get(`repos/${ newRepo }`);
+
+//         const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+//         if (hasRepo) {
+//           throw new Error('Repositorio Duplicado');
+//         }
+
+//         const data = {
+//           name: response.data.full_name,
+//         }
+
+//         setRepositorios([...repositorios, data]);
+//         setNewRepo('');
+//       } catch (error) {
+//         setAlert(true);
+//         console.log(error);
+//       } finally {
+//         setLoading(false);
+//       }
+
+//     }
+
+//     submit();
+
+//   }, [newRepo, repositorios]);
+
+//   function handleinputChange(e) {
+//     setNewRepo(e.target.value);
+//     setAlert(null);
+//   }
+
+//   const handleDelete = useCallback((repo) => {
+//     const find = repositorios.filter(r => r.name !== repo);
+//     setRepositorios(find);
+//   }, [repositorios]);
+
+
+//   return (
+//     <Container>
+
+//       <h1>
+//         <FaGithub size={ 25 } />
+//         Meus Repositorios
+//       </h1>
+
+//       <Form onSubmit={ handleSubmit } error={ alert }>
+//         <input
+//           type="text"
+//           placeholder="Adicionar Repositorios"
+//           value={ newRepo }
+//           onChange={ handleinputChange }
+//         />
+
+//         <SubmitButton loading={ loading ? 1 : 0 }>
+//           { loading ? (
+//             <FaSpinner color="#FFF" size={ 14 } />
+//           ) : (
+//             <FaPlus color="#FFF" size={ 14 } />
+//           ) }
+//         </SubmitButton>
+
+//       </Form>
+
+//       <List>
+//         { repositorios.map(repo => (
+//           <li key={ repo.name }>
+//             <span>
+//               <DeleteButton onClick={ () => handleDelete(repo.name) }>
+//                 <FaTrash size={ 14 } />
+//               </DeleteButton>
+//               { repo.name }
+//             </span>
+//             <a href="">
+//               <FaBars size={ 20 } />
+//             </a>
+//           </li>
+//         )) }
+//       </List>
+
+//     </Container>
+//   )
+// }
